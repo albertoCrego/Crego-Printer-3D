932e148b43a4621a4b5efa242d2caf20f4a57124
albertoCrego <43600437+albertoCrego@users.noreply.github.com>
2020-02-11 18:39:08 +0100

    Corrección pasos Eje X.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -731,7 +731,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 1066.67, 95.23 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 168.42, 1066.67, 95.23 }
 
 /**
  * Default Max Feed Rate (mm/s)
